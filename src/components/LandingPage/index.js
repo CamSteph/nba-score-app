e0c85718eccb98cbe96815ec45e0aa9992ee0a5d
CamSteph <camstephenson2017@gmail.com>
2022-11-22 12:48:29 -0500

    Update landing page layout

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -2,16 +2,44 @@ import React from 'react';
 import styled from 'styled-components';
 import { customStyles } from '../../utilities/customStyles';
 import CallToActionImage from '../../media/images/home_img.svg';
+import ScoresImage from '../../media/images/scores_img.jpg';
+import PlayerImage from '../../media/images/player_img.jpg';
+import { Link } from 'react-router-dom';
+import { BsBoxArrowUpRight } from 'react-icons/bs';
 
-const LandingPageContainer = styled.div`
+const Container = styled.main`
   width: 100%;
   min-height: 100vh;
   background: ${customStyles.medium_shade_01};
   padding: 120px 0 50px;
 `;
 
-const LandingPageTitle = styled.h1`
-  text-align: center;
+const TopWrapper = styled.section`
+  width: 100%;
+  height: auto;
+  text-align: left;
+  padding: 30px;
+  min-height: 35vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  @media ( max-width: 640px ) {
+    flex-direction: column-reverse;
+    text-align: center;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 60px;
+  line-height: 150%;
+`;
+
+const PageTitle = styled.h1`
   color: ${customStyles.light_shade_01};
 
   strong{
@@ -19,61 +47,119 @@ const LandingPageTitle = styled.h1`
   }
 `;
 
-const CardWrapper = styled.main`
-  width: 85%;
-  height: auto;
-  max-height: 170vh;
-  min-height: 45vh;
-  background: transparent;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(4, 1fr);
-  grid-column-gap: 50px;
-  margin: auto;
-  place-items: center;
-  /* padding-top: 80px; */
-`;
-
-const Card = styled.div`
-  width: 100%;
-  height: 100%;
-  min-height: 200px;
-  padding: ${props => props.callToAction ? '' : '20px'};
-  grid-column: ${props => `${props.startCol} / ${props.endCol}`};
-  grid-row: ${props => `${props.startRow} / ${props.endRow}`};
-  background: ${props => props.callToAction ? '' : 'rgba(255, 255, 255, .1)'};
-  border-radius: 6px;
+const PageMessage = styled.div`
   color: ${customStyles.light_shade_01};
-  overflow-y: scroll;
+  margin-top: 20px;
+  opacity: .7;
 
+  @media ( min-width: 1040px ) {
+    max-width: 80%;
+  }
 `;
 
-const CallToActionCard = styled.img`
-  object-fit: contain;
-  grid-column: ${props => `${props.startCol} / ${props.endCol}`};
-  grid-row: ${props => `${props.startRow} / ${props.endRow}`};
-  width: 90%;
-  height: 65%;
-  padding: 20px;
-  padding-top: 0;
-  position: relative;
+const TopImage = styled.img`
+  width: 50%;
+  height: 50%;
+
+  @media ( min-width: 1040px ) {
+    width: 40%;
+    height: 40%;
+  }
+`;
+
+const BottomWrapper = styled.section`
+  width: 100%;
+  height: auto;
+  min-height: 35vh;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: auto;
+  grid-gap: 20px;
+  place-items: center;
+  padding-top: 50px;
+
+  @media ( max-width: 640px ) {
+    grid-template-columns: 1fr;
+    padding-top: 25px;
+  }
+`;
+
+const ActionCard = styled.div`
+  width: 80%;
+  max-width: 380px;
+  height: 220px;
   border-radius: 6px;
+  padding: 20px;
+  color: ${customStyles.light_shade_01};
+  font-weight: bold;
+  background: rgba(255, 255, 255, .1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  overflow: hidden;
+
+  a {
+    width: 80%;
+
+    button {
+    width: 100%;
+    padding: 10px;
+    font-weight: bold;
+    cursor: pointer;
+    transition: all .5s ease;
+    
+    &:hover{
+      background: transparent;
+      border: 1px solid ${customStyles.light_shade_01};
+      color: ${customStyles.light_shade_01};
+    }
+  }
+}
+`;
+
+const ActionImage = styled.div`
+  width: 100%;
+  height: 65%;
+  object-fit: cover;
+  border-radius: 12px;
+  background: linear-gradient(to top, rgba(0, 0, 0, .4), rgba(0, 0, 0, .7)), url(${props => props.bgImg});
+  background-position: center ${props => props.bgPos};
+  background-size: cover;
+  display: grid;
+  place-items: center;
+  text-align: center;
 `;
 
 const LandingPage = () => {
   return (
-    <LandingPageContainer>
-      <LandingPageTitle>Welcome to the <strong>Player's</strong> League.</LandingPageTitle>
-      <CardWrapper>
-        <CallToActionCard src={CallToActionImage} startCol={1} endCol={-1} startRow={0} endRow={1} />
-        <Card startCol={1} endCol={4} startRow={3} endRow={3} bgColorVal='blue'>
-          LEARN MORE
-        </Card>
-        <Card startCol={4} endCol={-1} startRow={3} endRow={3} bgColorVal='green'>
-          HEADLINES & ARTICLES
-        </Card>
-      </CardWrapper>
-    </LandingPageContainer>
+    <Container>
+      <TopWrapper>
+        <Wrapper>
+          <PageTitle>Welcome to the <strong>Player's</strong> League.</PageTitle>
+          <PageMessage>Check out the most recent NBA games, your favorite players, and their season averages all in one hub!!</PageMessage>
+        </Wrapper>
+        <TopImage src={CallToActionImage} />
+      </TopWrapper>
+      <BottomWrapper>
+        <ActionCard>
+          <ActionImage bgImg={ScoresImage} bgPos='top'>
+            <span>Check out the most recent scores!</span>
+          </ActionImage>
+          <Link to='/scores'>
+            <button>View More <BsBoxArrowUpRight /></button>
+          </Link>
+        </ActionCard>
+        <ActionCard>
+          <ActionImage bgImg={PlayerImage} bgPos='top'>
+            <span>View your favorite players here!</span>
+          </ActionImage>
+          <Link to='/players'>
+            <button>View More <BsBoxArrowUpRight /></button>
+          </Link>
+        </ActionCard>
+      </BottomWrapper>
+    </Container>
   );
 };
 
